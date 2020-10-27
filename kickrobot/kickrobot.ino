54424ec8101b61e749b1f6cf66ed05e0ad1121bc
Kauê Rodrigues <kauerodrigues98@gmail.com>
2020-10-26 23:18:24 -0300

    jesus.

--- kickrobot/kickrobot.ino
+++ kickrobot/kickrobot.ino
@@ -1,12 +1,33 @@
+#include <Servo.h>
 
+#define PIN_SERVO_1 9
+#define PIN_SERVO_2 10
 
+#define FINAL_VALUE 90
+#define INITIAL_VALUE 0
+
+#define DELAY_VALUE 1000
+
+Servo servo1; 
+Servo servo2;
 
 void setup() {
-  // put your setup code here, to run once:
+  servo1.attach(PIN_SERVO_1);
+  servo2.attach(PIN_SERVO_2);
+  writeServoPin(INITIAL_VALUE, INITIAL_VALUE, false);
+}
 
+void writeServoPin(unsigned int servoMotor1Value, unsigned int servoMotor2Value, boolean withDelay) {
+  servo1.write(servoMotor1Value);
+  servo2.write(servoMotor2Value);
+  if (withDelay) delay(DELAY_VALUE);
+}
+
+void runFullCycle() {
+  writeServoPin(INITIAL_VALUE, FINAL_VALUE, true);
+  writeServoPin(FINAL_VALUE, INITIAL_VALUE, true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  runFullCycle();
 }
